a62c00f18e14ec666f729125271b51f320a0520b
setejera <45438505+setejera@users.noreply.github.com>
2021-09-12 11:18:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 setup(name='Periculum_Group_B_IE_GMBD',
       version='0.0.1',  # Development release
       description='Risk_Based_Segmentation Package, Group Assignment',
-      url='https://github.com/setejera/Periculum_Group_B.git',
+      url='https://github.com/setejera/Periculum_Group_B_IE_GMBD.git',
       author='Sebastian Tejera, Dora Jaber,  Alejandro Kreutzer, Parma Govindsamy, Caroline Ghazzaoui ',
       author_email='sebastian.tejera@student.ie.edu, parmagovindsamy@student.ie.edu, alejandro.kreutzer@student.ie.edu, dorajaber@student.ie.edu, caroline.ghazzaoui@student.ie.edu, setejera@gmail.com',
       license='MIT',
