4169a23f2d69f4072ae7e7999cc98d65f993325f
setejera <45438505+setejera@users.noreply.github.com>
2021-09-12 10:17:04 -0400

    Restarting the pypi process

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='Periculum_Group_B_IE',
+      version='0.0.1',  # Development release
+      description='Risk_Based_Segmentation Package, Group Assignment',
+      url='https://github.com/setejera/Periculum_Group_B.git',
+      author='Sebastian Tejera, Dora Jaber,  Alejandro Kreutzer, Parma Govindsamy, Caroline Ghazzaoui ',
+      author_email='sebastian.tejera@student.ie.edu, parmagovindsamy@student.ie.edu, alejandro.kreutzer@student.ie.edu, dorajaber@student.ie.edu,'
+                   'caroline.ghazzaoui@student.ie.edu', 'setejera@gmail.com',
+      license='MIT',
+      packages=['Periculum_Group_B_IE'],
+      zip_safe=False
+    )
