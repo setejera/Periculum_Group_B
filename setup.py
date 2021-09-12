25b3de05391f1a5f6020daba79a3f7195c99c022
Caroline Ghazzaoui <caroline-ghazzaoui@bluewin.ch>
2021-09-12 11:05:57 +0200

    first commit

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='Periculum_Group_B_IE',
       version='0.1',  # Development release
       description='Risk_Based_Segmentation Package, Group Assignment',
-      url='https://github.com/setejera/Periculum_Group_B.git',
-      author='Sebastian Tejera,Dora Jaber,  Alejandro Kreutzer, Parma Govindsamy, Caroline Ghazzaoui ',
+      url='https://github.com/setejera/Periculum_Group_B_IE.git',
+      author='Sebastian Tejera, Dora Jaber,  Alejandro Kreutzer, Parma Govindsamy, Caroline Ghazzaoui ',
       author_email='sebastiantejera@student.ie.edu, parmagovindsamy@student.ie.edu, alejandro.kreutzer@student.ie.edu, dorajaber@student.ie.edu, caroline.ghazzaoui@student.ie.edu',
       license='MIT',
-          packages=['Periculum_Group_B'],
+          packages=['Periculum_Group_B_IE'],
       zip_safe=False)
