cf623cd2b93bd89a538c4e28338953498156c6d5
setejera <45438505+setejera@users.noreply.github.com>
2021-09-12 10:43:24 -0400

    Wheel build

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ setup(name='Periculum_Group_B_IE',
       description='Risk_Based_Segmentation Package, Group Assignment',
       url='https://github.com/setejera/Periculum_Group_B.git',
       author='Sebastian Tejera, Dora Jaber,  Alejandro Kreutzer, Parma Govindsamy, Caroline Ghazzaoui ',
-      author_email='sebastian.tejera@student.ie.edu, parmagovindsamy@student.ie.edu, alejandro.kreutzer@student.ie.edu, dorajaber@student.ie.edu,'
-                   'caroline.ghazzaoui@student.ie.edu', 'setejera@gmail.com',
+      author_email='sebastian.tejera@student.ie.edu, parmagovindsamy@student.ie.edu, alejandro.kreutzer@student.ie.edu, dorajaber@student.ie.edu, caroline.ghazzaoui@student.ie.edu, setejera@gmail.com',
       license='MIT',
       packages=['Periculum_Group_B_IE'],
-      zip_safe=False
-    )
+      zip_safe=False,
+      )
